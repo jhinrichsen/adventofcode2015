f862dc16c1801c1bfcd0fbab65ec61bd6651bfd3
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-08-29 17:36:04 +0200

    feat: add day 12 part 2

--- day12.go
+++ day12.go
@@ -1,6 +1,7 @@
 package adventofcode2015
 
 import (
+	"encoding/json"
 	"strconv"
 	"strings"
 	"unicode"
@@ -30,4 +31,62 @@ func sum(s string) int {
 		total += n
 	}
 	return total
+}
+
+func sum2(s string) int {
+	buf := []byte(s)
+	// "the empty interface says nothing"
+	var data interface{}
+	if err := json.Unmarshal(buf, &data); err != nil {
+		return 0
+	}
+
+	numbers := make(chan int)
+	go walk(0, data, numbers)
+
+	var total int
+	for n := range numbers {
+		total += n
+	}
+	return total
+}
+
+func walk(depth int, v interface{}, numbers chan<- int) {
+	switch vv := v.(type) {
+	case string:
+		// ignore strings
+	case float64:
+		// fmt.Println(v, "is number", vv)
+		numbers <- int(vv)
+	case []interface{}:
+		// fmt.Println(v, "is an array:")
+		for _, v := range vv {
+			walk(depth+1, v, numbers)
+		}
+	case map[string]interface{}:
+		// fmt.Println(v, "is a map:")
+		if !hasRedProperty(vv) {
+			for _, v := range vv {
+				walk(depth+1, v, numbers)
+			}
+		}
+	default:
+		panic(vv)
+	}
+	// Once we're done, and back to root level, indicate end of numbers
+	if depth == 0 {
+		close(numbers)
+	}
+}
+
+func hasRedProperty(m map[string]interface{}) bool {
+	for _, v := range m {
+		switch t := v.(type) {
+		case string:
+			if t == "red" {
+				return true
+			}
+		}
+	}
+	return false
 }
--- day12_test.go
+++ day12_test.go
@@ -1,6 +1,7 @@
 package adventofcode2015
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -9,7 +10,7 @@ import (
 	"testing"
 )
 
-var day11Samples = []struct {
+var day12SamplesPart1 = []struct {
 	in  string
 	out int
 }{
@@ -21,6 +22,16 @@ var day11Samples = []struct {
 	{`[-1,{"a":1}]`, 0},
 	{`[]`, 0},
 	{`{}`, 0},
+}
+
+var day12SamplesPart2 = []struct {
+	in  string
+	out int
+}{
+	{`[1,2,3]`, 6},
+	{`[1,{"c":"red","b":2},3]`, 4},
+	{`{"d":"red","e":[1,2,3,4],"f":5}`, 0},
+	{`[1,"red",5]`, 6},
 }
 
 // Neither strconv.Atoi() nor fmt.Sscanf() support recovery of illegal reads.
@@ -47,12 +58,12 @@ func NoTestParse(t *testing.T) {
 	fmt.Printf("i=%d, n=%d, err=%+v\n", i, n, err)
 }
 
-func TestDay12Samples(t *testing.T) {
-	for _, tt := range day11Samples {
+func TestDay12SamplesPart1(t *testing.T) {
+	for _, tt := range day12SamplesPart1 {
 		id := tt.in
 		t.Run(id, func(t *testing.T) {
 			want := tt.out
-			got := sum(tt.in)
+			got := sum2(tt.in)
 			if want != got {
 				t.Fatalf("%q: want %d but got %d", id, want, got)
 			}
@@ -60,14 +71,61 @@ func TestDay12Samples(t *testing.T) {
 	}
 }
 
-func TestDay12Part1(t *testing.T) {
+func TestDay12Part1Sum1(t *testing.T) {
 	filename := "testdata/day12.txt"
 	buf, err := ioutil.ReadFile(filename)
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	const want = 111754
 	got := sum(string(buf))
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay12Part1Sum2(t *testing.T) {
+	filename := "testdata/day12.txt"
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	const want = 111754
+	got := sum2(string(buf))
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay12SamplesPart2(t *testing.T) {
+	for _, tt := range day12SamplesPart2 {
+		id := tt.in
+		t.Run(id, func(t *testing.T) {
+			want := tt.out
+			got := sum2(tt.in)
+			if want != got {
+				t.Fatalf("%q: want %d but got %d", id, want, got)
+			}
+		})
+	}
+}
+
+// This is where we really need some parsing.
+func TestDay12Part2(t *testing.T) {
+	filename := "testdata/day12.txt"
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// "The empty interface says nothing" says Rob. It says any says Ian.
+	var data interface{}
+	if err := json.Unmarshal(buf, &data); err != nil {
+		t.Fatal(err)
+	}
+	want := 65402
+	got := sum2(string(buf))
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
