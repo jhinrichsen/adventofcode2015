8797cad04e7b29eba5731baab6d91609f9a6db59
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-09-06 11:38:04 +0200

    feat: add day 17 part 2

--- day17.go
+++ day17.go
@@ -1,7 +1,8 @@
 package adventofcode2015
 
-// Day17 returns number of combinations that storage can be fit into capacities.
-func Day17(storage uint, capacities []uint) uint {
+// Day17Part1 returns number of combinations that storage can be fit into
+// capacities.
+func Day17Part1(storage uint, capacities []uint) uint {
 	var n uint
 	sum := func(ns []uint) uint {
 		var m uint
@@ -19,6 +20,7 @@ func Day17(storage uint, capacities []uint) uint {
 	return n
 }
 
+// PowerSet returns all combinations (including empty one) for given array.
 func PowerSet(original []uint) [][]uint {
 	powerSetSize := 1 << len(original)
 	result := make([][]uint, 0, powerSetSize)
@@ -36,4 +38,35 @@ func PowerSet(original []uint) [][]uint {
 		index++
 	}
 	return result
+}
+
+// Day17Part2 returns number of combinations that storage can be fit into
+// capacities.
+func Day17Part2(storage uint, capacities []uint) uint {
+	// maps holds number of entriees of length n
+	m := make(map[uint]uint)
+	sum := func(ns []uint) uint {
+		var m uint
+		for _, n := range ns {
+			m += n
+		}
+		return m
+	}
+
+	for _, perm := range PowerSet(capacities) {
+		if sum(perm) == storage {
+			l := uint(len(perm))
+			m[l]++
+			// fmt.Printf("m[%d]=%d\n", l, m[l])
+		}
+	}
+
+	// find smallest existing index, return number of occurences
+	for i := uint(1); i < storage; i++ {
+		if n, ok := m[i]; ok {
+			return n
+		}
+	}
+	// this is unreachable code, but instead of panic() we
+	return 0
 }
--- day17_test.go
+++ day17_test.go
@@ -5,33 +5,64 @@ import (
 	"testing"
 )
 
+const storage = 150
+
+var exampleCapacities = []uint{
+	20, 15, 10, 5, 5,
+}
+
 func TestDay17Example(t *testing.T) {
 	const want = 4
 	const storage = 25
-	capacities := []uint{
-		20, 15, 10, 5, 5,
-	}
-	got := Day17(storage, capacities)
+	got := Day17Part1(storage, exampleCapacities)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
-func TestDay17Part1(t *testing.T) {
-	const want = 1304
-	lines, err := linesFromFilename(filename(17))
-	if err != nil {
-		t.Fatal(err)
-	}
+func numbers(filename string) ([]uint, error) {
 	var ns []uint
+	lines, err := linesFromFilename(filename)
+	if err != nil {
+		return ns, err
+	}
 	for _, line := range lines {
 		n, err := strconv.ParseUint(line, 10, 32)
 		if err != nil {
-			t.Fatal(err)
+			return ns, err
 		}
 		ns = append(ns, uint(n))
 	}
-	got := Day17(150, ns)
+	return ns, nil
+}
+
+func TestDay17Part1(t *testing.T) {
+	const want = 1304
+	ns, err := numbers(filename(17))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day17Part1(storage, ns)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay17Part2Example(t *testing.T) {
+	const want = 3
+	got := Day17Part2(25, exampleCapacities)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay17Part2(t *testing.T) {
+	const want = 18
+	ns, err := numbers(filename(17))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day17Part2(storage, ns)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
