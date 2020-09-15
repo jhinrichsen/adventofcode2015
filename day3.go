7c43cd3c3eabaa0eb37d26ba79257717c31f3211
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-09-15 19:30:04 +0200

    refactor: day 3 Rust -> Go

---
+++ day3.go
@@ -0,0 +1,53 @@
+package adventofcode2015
+
+type position [2]int
+
+// Day3Part1 returns number of fields visited.
+func Day3Part1(buf []byte) uint {
+	houses := make(map[position]bool)
+	pos := position{0, 0}
+	houses[pos] = true
+	for _, b := range buf {
+		pos = move(pos, b)
+		houses[pos] = true
+	}
+	return uint(len(houses))
+}
+
+func move(pos position, b byte) position {
+	switch b {
+	case '>':
+		pos[0]++
+	case '<':
+		pos[0]--
+	case 'v':
+		pos[1]++
+	case '^':
+		pos[1]--
+	}
+	return pos
+}
+
+// Day3Part2 TODO.
+func Day3Part2(buf []byte) uint {
+	houses := make(map[position]bool)
+	// index santa = 0, robo santa = 1
+	poss := []position{
+		{0, 0},
+		{0, 0},
+	}
+	// santa starts
+	who := 0
+	mark := func() {
+		houses[poss[who]] = true
+	}
+	// deliver to current position
+	mark()
+	for _, b := range buf {
+		poss[who] = move(poss[who], b)
+		mark()
+		// take turns
+		who = 1 - who
+	}
+	return uint(len(houses))
+}
---
+++ day3_test.go
@@ -0,0 +1,82 @@
+package adventofcode2015
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+var day3ExamplesPart1 = []struct {
+	in  string
+	out uint
+}{
+	{">", 2},
+	{"^>v<", 4},
+	{"^v^v^v^v^v", 2},
+}
+
+func TestDay3ExamplesPart1(t *testing.T) {
+	for _, tt := range day3ExamplesPart1 {
+		id := tt.in
+		t.Run(id, func(t *testing.T) {
+			want := tt.out
+			got := Day3Part1([]byte(tt.in))
+			if want != got {
+				t.Fatalf("want %d but got %d", want, got)
+			}
+		})
+	}
+}
+
+func inputDay3() (string, error) {
+	buf, err := ioutil.ReadFile(filename(3))
+	if err != nil {
+		return "", err
+	}
+	return string(buf), nil
+}
+
+func TestDay3Part1(t *testing.T) {
+	const want = 2081
+	s, err := inputDay3()
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day3Part1([]byte(s))
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+var day3ExamplesPart2 = []struct {
+	in  string
+	out uint
+}{
+	{"^>", 3},
+	{"^>v<", 3},
+	{"^v^v^v^v^v", 11},
+}
+
+func TestDay3ExamplesPart2(t *testing.T) {
+	for _, tt := range day3ExamplesPart2 {
+		id := tt.in
+		t.Run(id, func(t *testing.T) {
+			want := tt.out
+			got := Day3Part2([]byte(tt.in))
+			if want != got {
+				t.Fatalf("want %d but got %d", want, got)
+			}
+		})
+	}
+}
+
+func TestDay3Part2(t *testing.T) {
+	const want = 2341
+	s, err := inputDay3()
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day3Part2([]byte(s))
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
