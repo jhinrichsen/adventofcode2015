5af46d8efa2df2415f6a9b134284e1218a0cdfd6
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-09-27 14:16:27 +0200

    feat: add day 21 part 2

--- day21.go
+++ day21.go
@@ -5,22 +5,32 @@ import (
 )
 
 type player struct {
-	hitPoints     int
-	damage, armor uint
+	hitPoints int  // also known as life, score
+	items     item // all the gear he is carrying (one or more)
 }
 
 func (a player) alive() bool {
 	return a.hitPoints > 0
 }
 
-// Day21Part1 plays until one player dies.
-func Day21Part1(players *[2]player) {
+var boss = player{
+	hitPoints: 100,
+	items: item{
+		damage: 8,
+		armor:  2,
+	},
+}
+var nakedMe = player{100, item{0, 0, 0}}
+
+// duel plays until one player dies.
+func duel(players *[2]player) {
 	attack := 0 // player 1 begins
 	turn := func() {
 		attack = 1 - attack
 	}
 	for {
-		damage := players[attack].damage - players[1-attack].armor
+		damage := players[attack].items.damage -
+			players[1-attack].items.armor
 		if damage < 1 {
 			damage = 1
 		}
@@ -33,7 +43,7 @@ func Day21Part1(players *[2]player) {
 }
 
 type item struct {
-	cost, damage, armor uint
+	cost, damage, armor int
 }
 
 func (a *item) add(i item) {
@@ -42,6 +52,7 @@ func (a *item) add(i item) {
 	a.armor += i.armor
 }
 
+// Must always have one weapon, even in part 2.
 var weaponItems = []item{
 	{8, 4, 0},  // Dagger
 	{10, 5, 0}, // Shortsword
@@ -51,6 +62,7 @@ var weaponItems = []item{
 }
 
 var armorItems = []item{
+	{0, 0, 0},   // No armor
 	{13, 0, 1},  // Leather
 	{31, 0, 2},  // Chainmail
 	{53, 0, 3},  // Splitmail
@@ -59,6 +71,7 @@ var armorItems = []item{
 }
 
 var ringItems = []item{
+	{0, 0, 0},   // No ring
 	{25, 1, 0},  // Damage +1
 	{50, 2, 0},  // Damage +2
 	{100, 3, 0}, // Damage +3
@@ -67,32 +80,25 @@ var ringItems = []item{
 	{80, 0, 3},  // Defense +3
 }
 
-// combinations returns array sorted by cost asc.
-func combinations() []item {
+// itemCombinations returns array sorted by cost asc.
+// i got part 2 wrong, it seems as if one cannot carry any combination, but
+// still need exactly one weapon.
+func itemCombinations() []item {
 	m := make(map[item]bool, 5*6*15)
 	// 1 weapon
 	for _, weapon := range weaponItems {
-		// 0..1 armor
-		for a := 0; a <= 1; a++ {
-			for _, armor := range armorItems {
-				// 0..2 rings
-				for r := 0; r <= 2; r++ {
-					for _, ring1 := range ringItems {
-						for _, ring2 := range ringItems {
-							w := weapon
-							if a == 1 {
-								w.add(armor)
-							}
-							if r == 1 {
-								w.add(ring1)
-							}
-							// cannot use the same ring twice
-							if r == 2 && (ring1 != ring2) {
-								w.add(ring2)
-							}
-							m[w] = true
-						}
+		for _, armor := range armorItems {
+			for _, ring1 := range ringItems {
+				for _, ring2 := range ringItems {
+					// cannot use the same ring twice
+					if ring1 == ring2 {
+						continue
 					}
+					w := weapon
+					w.add(armor)
+					w.add(ring1)
+					w.add(ring2)
+					m[w] = true
 				}
 			}
 		}
@@ -102,9 +108,50 @@ func combinations() []item {
 	for k := range m {
 		cs = append(cs, k)
 	}
+	return cs
+}
+
+// Day21Part1 returns minimal cost to survive.
+func Day21Part1() int {
+	cs := itemCombinations()
 	// sort by cost asc
 	sort.Slice(cs, func(i, j int) bool {
 		return cs[i].cost < cs[j].cost
 	})
-	return cs
+	for _, c := range cs {
+		p := nakedMe
+		p.items.armor = c.armor
+		p.items.damage = c.damage
+		players := [...]player{
+			p,
+			boss,
+		}
+		duel(&players)
+		if players[0].alive() {
+			return c.cost
+		}
+	}
+	return 0
+}
+
+// Day21Part2 returns highest cost so that player 0 still loses.
+func Day21Part2() int {
+	cs := itemCombinations()
+	// sort by cost desc
+	sort.Slice(cs, func(i, j int) bool {
+		return cs[i].cost > cs[j].cost
+	})
+	for _, items := range cs {
+		p := nakedMe
+		p.items = items
+		players := [...]player{
+			p,
+			boss,
+		}
+		duel(&players)
+		if !players[0].alive() {
+			return players[0].items.cost
+		}
+	}
+	return 0
 }
